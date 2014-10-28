f3d748292078b536c223612e065bb8792259668b
sami <sami@sami-HP-530-Notebook-PC.(none)>
2014-10-28 04:16:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+jQuery routr
+============
+
+## Quick Start
+
+Why another javascript router? The more the merrier. :)
+
+```js
+	$.routr.add("user/{int}", function(id){
+
+		console.log("User::id->".concat(id));
+	})
+
+	$.routr.add("user/add", function(){
+
+		console.log("User::add");
+	})
+
+	$.routr.run();
+```
+
+The above functions would be accessed through:
+
+1. **localhost/app#user/1**
+2. **localhost/app#user/add**
+
+## Usage
+
+Functions:
+
+1. Adding route
+```js
+$.routr.add("user/{string}", function(username){})
+```
+2. Removing route
+```js
+$.routr.remove("user/{string}")
+```
+3. Execute route
+```js
+$.routr.execute("/user/add")
+```
+4. Start listening for routes (uses ***hashchange*** state)
+```js
+$.routr.run();
+```
+
+## Route Parameters
+
+1. **int** - positive numbers
+2. **float** - signed numbers
+3. **date** - format *yyyy-mm-dd*
+4. **string** - alpha numeric characters
+5. **bool** - *true* or *false*
+
+## License
+
+Copyright (c) 2014 Samuel Weru
+Licensed under the MIT license.
---
+++ jquery.routr.js
@@ -0,0 +1,115 @@
+/*
+ * jQuery routr - v0.9 beta
+ * Javascript Router
+ *
+ * Copyright (c) 2014 Samuel Weru
+ * Released under the MIT license
+ */
+String.prototype.test = function(expr) {
+    
+    return (new RegExp(expr)).test(this.toString());
+};
+
+(function ($){
+ 
+    $.routr = $.extend({}, {routes:[], aliases:[], current:""});
+
+    $.routr.add = function(route, alias, callback){
+
+        if(typeof alias === "function") {
+
+            callback = alias; 
+            alias = undefined
+        }
+
+        if(alias === undefined) 
+            alias = "route".concat(Object.keys($.routr.routes).length);
+
+        if($.routr.routes[route] === undefined)
+            $.routr.routes[route] = [];
+
+        var parts = [];
+        var pattern = [];
+        if(route.match(/\{/)){
+
+            parts = route.split("/");
+            $.each(parts, function(i,e){
+
+                var shards = e.match(/\{(\w+)(?:\:(int|float|date|string|bool)\}|\})/i);
+                if(shards)
+                    switch(shards[1]||shards[2]){
+
+                        case "int":
+                            pattern.push("[0-9]+")
+                        break;
+                        case "float":
+                            pattern.push("[+-]?\d+(\.\d+)?")
+                        break;
+                        case "date":
+                            //yyyy-mm-dd
+                            pattern.push("[0-9]{4}-(1[0-2]|0[1-9]|[1-9])-([0-9]|0[1-9]|[1-2][0-9]|3[0-1])")
+                        break;
+                        case "string":
+                            pattern.push("[A-Za-z0-9]+")
+                        break;
+                        case "bool":
+                            pattern.push("(true|false)")
+                        break;
+                    }
+                else
+                    pattern.push(e)
+            });
+        }
+
+        $.routr.aliases[alias] = route;
+        $.routr.routes[route] = {"route":route, 
+                                    "callback":callback,
+                                    "alias":alias,
+                                    "pattern":pattern.join("/")};
+    }
+
+    $.routr.remove = function(route){
+
+        var alias = $.routr.routes[route].alias;
+        delete $.routr.aliases[alias]
+        delete $.routr.routes[route]
+    }
+
+    $.routr.execute = function(route){
+
+        $.routr.current = route;
+        if($.routr.routes[route] === undefined)
+            $.each(Object.keys($.routr.routes), function(i,e){
+
+                var params = [];
+                var pattern = $.routr.routes[e].pattern;
+                if(route.test("^".concat(pattern.concat("$")))){
+
+                    var parts = e.split("/"), 
+                        _parts = route.split("/"),
+                        x;
+
+                    for(x in parts)
+                        if(parts[x] != _parts[x])
+                            params.push(_parts[x]);
+
+                    if(params.length)    
+                        $.routr.routes[e].callback.apply(this, params);
+                       
+                    return false;
+                }
+            });
+        else
+            $.routr.routes[route].callback();
+    }
+
+    $.routr.run = function(){
+
+        if(Object.keys($.routr.routes).length)
+            $(window).bind('hashchange', function() {
+
+                $.routr.execute(window.location.hash.replace(/#\/|#/g,""));
+            });
+    }
+ 
+}(jQuery));
